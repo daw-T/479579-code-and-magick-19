4b82b0b64dc28766d3801912f243f0ba7afcbfd3
daw-T <44907469+daw-T@users.noreply.github.com>
2020-01-30 20:37:20 +0300

    добавляет отдельные функции для отрисовки текста и столбиков диаграммы, убирает функцию сравнения массивов

--- js/stat.js
+++ js/stat.js
@@ -10,9 +10,15 @@ var TEXT_HEIGHT = 15;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 
-var renderCloud = function (ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, width, height, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, width, height, color);
+};
+
+var renderText = function (ctx, text, x, y, color, font) {
+  ctx.fillStyle = color;
+  ctx.font = font;
+  ctx.fillText(text, x, y);
 };
 
 var getMaxElement = function (arr) {
@@ -44,46 +50,26 @@ var getColorforBar = function (namePlayer) {
   return getHsl(Math.random() * (250 - 180) + 180, Math.random() * (100 - 50) + 50, 50);
 };
 
+var renderBar = function (ctx, x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height, color);
+};
+
 window.renderStatistics = function (ctx, names, times) {
-  var comparesArrays = function () {
-    if (names.length === times.length) {
-      return;
-    }
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#fff');
 
-    if (names.length > times.length) {
-      names = names.slice(0, times.length);
-    } else {
-      times = times.slice(0, names.length);
-    }
-  };
-
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура, вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT);
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + (GAP + TEXT_HEIGHT) * 2);
-
-  comparesArrays();
+  renderText(ctx, 'Ура, вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT, '#000', '16px PT Mono');
+  renderText(ctx, 'Список результатов:', CLOUD_X + GAP, CLOUD_Y + (GAP + TEXT_HEIGHT) * 2, '#000', '16px PT Mono');
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.font = '16px PT Mono';
-    ctx.fillText(names[i], CLOUD_X + GAP_BETWEEN_BAR + (BAR_WIDTH + GAP_BETWEEN_BAR) * i, CLOUD_Y + GAP + (TEXT_HEIGHT + GAP) * 3 + BAR_HEIGHT + GAP);
+    renderText(ctx, names[i], CLOUD_X + GAP_BETWEEN_BAR + (BAR_WIDTH + GAP_BETWEEN_BAR) * i, CLOUD_Y + GAP + (TEXT_HEIGHT + GAP) * 3 + BAR_HEIGHT + GAP, '#000', '16px PT Mono');
 
-    var differenceHeightMaxBarEndBar = (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime);
+    var differenceHeightMaxBarEndBar = (BAR_HEIGHT - BAR_HEIGHT * times[i] / maxTime);
 
-    ctx.fillStyle = getColorforBar(names[i]);
-    ctx.fillRect(CLOUD_X + GAP_BETWEEN_BAR + (BAR_WIDTH + GAP_BETWEEN_BAR) * i, CLOUD_Y + (TEXT_HEIGHT + GAP) * 3 + differenceHeightMaxBarEndBar, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
-
-    ctx.fillStyle = '#000';
-    ctx.font = '16px PT Mono';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP_BETWEEN_BAR + (BAR_WIDTH + GAP_BETWEEN_BAR) * i, CLOUD_Y + GAP * 2 + (TEXT_HEIGHT + GAP) * 2 + differenceHeightMaxBarEndBar);
+    renderBar(ctx, CLOUD_X + GAP_BETWEEN_BAR + (BAR_WIDTH + GAP_BETWEEN_BAR) * i, CLOUD_Y + (TEXT_HEIGHT + GAP) * 3 + differenceHeightMaxBarEndBar, BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime, getColorforBar(names[i]));
+    renderText(ctx, Math.round(times[i]), CLOUD_X + GAP_BETWEEN_BAR + (BAR_WIDTH + GAP_BETWEEN_BAR) * i, CLOUD_Y + GAP * 2 + (TEXT_HEIGHT + GAP) * 2 + differenceHeightMaxBarEndBar, '#000', '16px PT Mono');
   }
 };
